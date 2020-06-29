824619ee4207182710bfa978178163761faca978
ibrahim suleiman <ibrahimsuleiman364@gmail.com>
2020-06-30 00:57:30 +0100

    Fixed some bug and added avater fetching function

--- src/components/navbar/SignedInMenu.js
+++ src/components/navbar/SignedInMenu.js
@@ -1,35 +1,55 @@
-import React, { Fragment } from 'react';
-import { Menu, Image, Header, Dropdown } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Menu, Image, Dropdown } from 'semantic-ui-react';
 import { useSelector, useDispatch } from 'react-redux';
 import image from '../../user.jpg';
 import { logout } from '../../store/actions/authActions';
 
 const SignedInMenu = () => {
   const user = useSelector((state) => state.auth.loginData.user);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const [avatar, setAvatar] = useState();
+
+  useEffect(() => {
+    const getProfilePic = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3001/users/${user._id}/avatar`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'image/png',
+            },
+          }
+        );
+
+        const resData = await response.blob();
+
+        setAvatar(URL.createObjectURL(resData));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProfilePic();
+  }, [user._id]);
 
   return (
     <Menu.Item position='right'>
-      <Image src={user.avatar || image} size='mini' circular/>
+      <Image src={avatar || image} size='mini' circular />
       <Dropdown pointing='top left' text={user.name}>
         <Dropdown.Menu>
-          <Dropdown.Item 
-          onClick={() =>{
-            dispatch(logout)
-          }}
-          text='Sign Out'
-           />
+          <Dropdown.Item
+            onClick={() => {
+              dispatch(logout());
+            }}
+            text='Sign Out'
+          />
         </Dropdown.Menu>
       </Dropdown>
     </Menu.Item>
-    // <Fragment>
-    //   <Menu.Item position='right'>
-    //     <Image src={user.avatar || image} size='mini' circular />
-    //   </Menu.Item>
-    //   <Menu.Item position='right'>
-    //     <Header size='tiny'>{user.name}</Header>
-    //   </Menu.Item>
-    // </Fragment>
   );
 };
+
 export default SignedInMenu;
